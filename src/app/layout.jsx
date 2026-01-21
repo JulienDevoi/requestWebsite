f92acf58fcd61682de67e799fd132dd10779f092
JulienDevoi <julien.devoir@request.finance>
2026-01-21 21:47:04 +0000

    adwords converson pageVieweD

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -79,18 +79,6 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-708726534');
           `}
         </Script>
-        {/* Event snippet for ViewedPage Book conversion page */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              gtag('event', 'conversion', {
-                'send_to': 'AW-708726534/MrztCMTa-ukbEIae-dEC',
-                'value': 1.0,
-                'currency': 'EUR'
-              });
-            `,
-          }}
-        />
         <link
           rel="stylesheet"
           href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
